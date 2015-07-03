2b3b7a9b5e8cbf012a6f04a00dea735e3456d371
fgm2 <fgm2@cin.ufpe.br>
2015-07-03 17:39:06 +0000

    #9 Funcionalidade users: Criando o model, controller e views

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
