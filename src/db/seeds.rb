5ca0c9c4e653800d420696714b72b6a7da532ef4
fgm2 <fgm2@cin.ufpe.br>
2015-07-03 20:40:12 +0000

    Adicionando usuário padrão

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+userss = [
+            {:name => 'admin', :email => 'admin@reforco.com.br',:password => 'mprof2014', :password_confirmation =>'mprof2014'},
+  	    ]
+
+userss.each do |u|
+  User.create!(u)
+end
