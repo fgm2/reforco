0566adb31e8553d78f5898343ad58f73e4c4dfe4
fgm2 <fgm2@cin.ufpe.br>
2015-07-05 03:28:54 +0000

    17 Reorganizando configurações dos modelos para suas próprias classes

--- src/config/initializers/rails_admin.rb
+++ src/config/initializers/rails_admin.rb
@@ -3,35 +3,18 @@ RailsAdmin.config do |config|
   config.main_app_name = ["Reforço", "MPROF"]
   config.main_app_name = Proc.new { |controller| [ "Reforço", "MPROF - #{controller.params[:action].try(:titleize)}" ] }
 
-
-  config.model "User" do
-      label "Usuário" 
-      label_plural "Usuários"
-      edit do
-        field :name
-        field :email
-        field :password
-        field :password_confirmation
-      end
-      list do
-        field :name
-        field :email
-        field :password
-      end
-  end
-    
    config.actions do
     dashboard
     index
     new
-    export
+    # export
     bulk_delete
     show
     edit
     delete
-    show_in_app
+    # show_in_app
     #history_index
-    history_show
+    #history_show
   end
   
   config.authenticate_with do
