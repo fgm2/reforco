b11403434a878fe5a8dfee3985e05ec713724faa
Fabiano Matos <fabiano.matos@live.com>
2015-07-08 20:57:36 +0000

    reconfigurando controller defaul pages e suas views

--- src/app/controllers/pages_controller.rb
+++ src/app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
-  def home
+  
+  def index
+    @teachers = Teacher.all
   end
+  
+  def show
+
+  end
+  
+  def sobre
+    # render 'pages/sobre'
+  end
+  
+  def termos
+    # render 'pages/termos'
+  end
+  
 end
