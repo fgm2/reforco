959e9df183421f3a774e396e26a569e0b449585f
fgm2 <fgm2@cin.ufpe.br>
2015-07-08 22:26:59 +0000

    Corrigindo a configuração do model user na area administrativa

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :teachers, dependent: :destroy
-  has_many :students, dependent: :destroy
+  # has_many :teachers, dependent: :destroy
+  # has_many :students, dependent: :destroy
   has_attached_file :avatar, styles: { medium: "300x300>"}#, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
          
   RailsAdmin.config do |config|
-        config.model "Student" do
-            label "Aluno"
-            label_plural "Alunos"
-            parent User
-            configure :user do
-              label 'Usuário'
+        config.model "User" do
+            label "Usuário"
+            label_plural "Usuários"
+            configure :name do
+              label 'Nome'
+            end
+            configure :email do
+              label 'E-mail'
             end
             configure :created_at do
                 label 'Criado em'
@@ -22,7 +24,8 @@ class User < ActiveRecord::Base
             end
             list do
               field :id
-              field :user
+              field :name
+              field :email
               field :created_at
               field :updated_at
             end
