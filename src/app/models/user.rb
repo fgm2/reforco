190edcf209aa5be55311218ea370c72a3d99ab44
Fabiano Matos <fabiano.matos@live.com>
2015-07-09 04:53:27 +0000

    adicionando novo tamanho de avatar: thumb in 'model/user.rb' e criados pasta/arquivo 'public/thumb/missing.png'

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   # has_many :teachers, dependent: :destroy
   # has_many :students, dependent: :destroy
-  has_attached_file :avatar, styles: { medium: "300x300>"}#, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, styles: {thumb: "150x150>",  medium: "300x300>"}#, :default_url => "/images/:style/missing.png"
   
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
          
