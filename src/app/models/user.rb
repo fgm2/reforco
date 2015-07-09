191f44832cca94f6a36d78b823970ded02d9d3ae
Fabiano Matos <fabiano.matos@live.com>
2015-07-09 03:22:54 +0000

    removendo caracteres invalidos do model 'user.rb'

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,15 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-<<<<<<< HEAD
-  #has_many :teachers, dependent: :destroy#, inverse_of: :user
-  #has_many :students, dependent: :destroy
-  #has_attached_file :avatar, styles: { thumb: "150x150>", medium: "300x300#"}#, :default_url => "/images/:style/missing.png"
-=======
+
   # has_many :teachers, dependent: :destroy
   # has_many :students, dependent: :destroy
   has_attached_file :avatar, styles: { medium: "300x300>"}#, :default_url => "/images/:style/missing.png"
->>>>>>> c99bde258dcb314d7da8f49c337666e28bab226e
+  
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
          
   RailsAdmin.config do |config|
