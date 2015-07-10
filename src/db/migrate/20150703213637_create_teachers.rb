c6b20fb1ed10262288f702559094567b91b174e3
fgm2 <fgm2@cin.ufpe.br>
2015-07-10 18:22:25 +0000

    Adicionando campo university no model teacher

--- src/db/migrate/20150703213637_create_teachers.rb
+++ src/db/migrate/20150703213637_create_teachers.rb
@@ -2,6 +2,7 @@ class CreateTeachers < ActiveRecord::Migration
   def change
     create_table :teachers do |t|
       t.string :formation
+      t.string :university
       t.string :description
       t.references :user, index: true, foreign_key: true
       t.datetime :created_at
