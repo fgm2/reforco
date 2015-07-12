ac4725507447c5482f7b88192d814cc55f6c7c35
Fabiano Matos <fabiano.matos@live.com>
2015-07-12 08:30:48 +0000

    criacao de metodos auxiliares no 'application_controller'

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
+  
+  def is_student(id)
+    Student.where("user_id = ? ", id).select(:id).count == 1
+  end
+  
+  def is_teacher(id)
+    Teacher.where("user_id = ? ", id).select(:id).count == 1
+  end
+  
+  helper_method :is_student
+  helper_method :is_teacher
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+
+  
   protected
 
   def configure_permitted_parameters
