9e2d5cd93931d15331ad8a4d0912fd44dfb46386
fgm2 <fgm2@cin.ufpe.br>
2015-07-22 16:51:00 +0000

    Alterações no controller users e criação do metodo meuperfil para tratar as informações da view meuperfil

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,12 +1,61 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
-  before_filter :require_admin, except: [:show, :edit, :update]
+  before_filter :require_admin, except: [:show, :edit, :update, :meuperfil]
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+  end
+  
+  def meuperfil
+    
+    @user = current_user
+    
+    if is_student(current_user.id) 
+      @student = getStudent(current_user.id)
+      @matriculas_student = Enrollment.where( "student_id = ? ", @student.id)
+      @matriculas_student.each do |aula_student|
+        @aulas_student = aula_student.course.name 
+      end
+    end
+    
+    if is_teacher(current_user.id) 
+      @teacher = getTeacher(current_user.id)
+      @aulas = Course.where( "teacher_id = ? ", @teacher.id)
+      @ultimas_aulas = Enrollment.where("course_id in ( ? )", @aulas.select(:id) ).limit(5)
+      @aulas_realizadas = Enrollment.where("id NOT IN ( ? ) AND course_id in ( ? )", @ultimas_aulas.select(:id),@aulas.select(:id) )
+      @minhas_aulas = Enrollment.where("course_id in ( ? )", @aulas.select(:id) )
+     
+      @horas_aulas = 0
+      @minhas_aulas.each do |aula|
+        @horas_aulas = @horas_aulas + aula.hours 
+      end
+      @cursos ||= Course.all
+      @matriculas ||= Enrollment.all
+      @recomendacoes ||= Recommendation.all
+  		
+  		@positivas = @recomendacoes.where("rating = 1 AND enrollment_id IN (?) ",
+                                              @matriculas.where("course_id IN (?)",
+                                                @cursos.where("teacher_id = ? ", @teacher.id).select(:id)
+                                                    ).select(:id) 
+                                                      ).count
+  		
+  		@negativas = @recomendacoes.where("rating = 0 AND enrollment_id IN (?) ",
+                                              @matriculas.where("course_id IN (?)",
+                                                @cursos.where("teacher_id = ? ",@teacher.id).select(:id)
+                                                ).select(:id)).count
+           
+           
+      if (@positivas+@negativas > 0)                                           
+        @porcentagem = (@positivas*100)/(@positivas+@negativas)
+      else
+        @porcentagem = 0
+      end
+    end
+    
+    
   end
 
   # GET /users/1
